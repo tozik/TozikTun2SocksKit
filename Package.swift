47f82fdd0a2bb76da8d7d6504e7e559b7b6e7722
Aleksander <tozika@inbox.ru>
2023-11-22 14:42:51 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ),
     .binaryTarget(
         name: "HevSocks5Tunnel",
-        url: "https://github.com/arror/Tun2SocksKit/releases/download/2.6.4/HevSocks5Tunnel.xcframework.zip",
+        url: "https://github.com/tozik/TozikTun2SocksKit/releases/download/2.6.4/HevSocks5Tunnel.xcframework.zip",
         checksum: "5e5d97cfa17fc28dfac542e1483a92275c2f4807a03e58993ea87a5270aa66da"
     )
   ]
